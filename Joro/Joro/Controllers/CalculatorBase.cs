e756137e58c52f230544ca3a278e7979441feec1
dyakovg <34554774+dyakovg@users.noreply.github.com>
2018-05-17 14:32:15 +0300

    small changes

---
+++ Joro/Joro/Controllers/CalculatorBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Joro.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default/Index?1=value&b=value
+        public ActionResult Index(int a = 0, int b= 0)
+        {
+            ViewBag.Title = "Примерно съдържание";
+
+            ViewBag.p = Request.Params;
+
+            ViewBag.a =a; //a 
+            ViewBag.b =b; //b
+            ViewBag.result = ViewBag.a + ViewBag.b;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            ViewBag.p = Request.Params;
+            ViewBag.a = Int32.Parse(Request.Params["a"]);
+            ViewBag.b = Int32.Parse(Request.Params["b"]);
+            ViewBag.resultat = ViewBag.a + ViewBag.b;
+            return View();
+        }
+    }
+    
+}
